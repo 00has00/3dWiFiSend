0c8f980e8e246b3ce8f38db29288ec2d66a805ec
00has00 <hugo@steinerit.com.au>
2018-10-14 10:36:15 +1100

    - Extensive rework - able to print to most FlashForge printers via Network.

--- 3DWiFiSendFile.py
+++ 3DWiFiSendFile.py
@@ -1,233 +1,261 @@
-#!/usr/bin/python3
+#! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import sys
 import logging
-import time
-from socket import *
-import struct
+import socket
+import binascii
+import argparse
+from pathlib import Path
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.WARN)
 logger = logging.getLogger(__name__)
 
-import re
 
-import subprocess
-import os
-from os import path
+class Print3D:
 
-class WiFiDevice():
+    # TODO: retrofit all routines that write to the network with responseOK()
+    # TODO: Really need some proper error & exception checking all the way thorugh
+
     CMD_STARTWRITE_SD = "M28 "
-    CMD_ENDWRITE_SD = "M29 "
-    CMD_GETFILELIST = "M20 "
-    PORT = 3000
+    CMD_ENDWRITE_SD = "~M29"
+    CMD_GETFILELIST = "~M20 "
+    CMD_STARTPRINT = "~M27"
+    CMD_SETSDFILE = "~M23 "
+    CMD_STATUS = "~M115\x0d\x0a"
+    CMD_INIT = "~M601 S1\x0d\x0a"
+    CMD_TERM = "\x0d\x0a"
+    CMD_RELEASE = "~M602\x0d\x0a"
+    CHUNK_HEADER1 = bytes([0x5a, 0x5a, 0xa5, 0xa5])
+    CHUNK_HEADER2 = bytes([0x00, 0x00, 0x10, 0x00])
+    RESPONSE_OK = 'ok\x0d\x0a'
+
     def __init__(self):
         self.ipaddr = ''
+        self.port = 8899
         self.name = 'undefined'
-        self.BUFSIZE = 256 * 5
-        self.RECVBUF = 256 * 5
-        self.gcodeFile = 'data.gcode'
-        self.fileName = 'data.gcode.tz'
+        self.PKTSIZE = 1460
+        self.BUFSIZE = 4113
+        self.RECVBUF = 1024
+        self.fileName = ''
+        self.baseFilename = ''
         self.dirPath = ''
-        self.sock = socket(AF_INET, SOCK_DGRAM)
-        self.sock.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
-        self.sock.settimeout(3)
+        self.sdPath = '0:/user/'
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._file_encode = 'utf-8'
 
     def __str__(self):
         s = ('Device addr:' + self.ipaddr + '==' + self.name)
         return s
 
-    def encodeCmd(self,cmd):
+    def connect(self, hostname='localhost'):
+        try:
+            self.ipaddr = socket.gethostbyname(hostname)
+            logger.info("Connected to host: {0}, IP: {1}".format(hostname, self.ipaddr))
+            self.sock.connect((self.ipaddr, self.port))
+            self.sock.send(bytes(self.CMD_INIT, self._file_encode))
+            msg = self.responseWait()
+            logger.debug(msg)
+            if not self.responseOK(msg):
+                logger.error("connect: Failed to control printer.")
+                return False
+            return True
+
+        except Exception as dd:
+            print(dd)
+            # print("Failed TCP connection to Server!")
+            exit(-1)
+
+        return
+
+    def release(self):
+        cmd = self.CMD_RELEASE
+        self.sock.send(self.encodeCmd(cmd))
+        msg = self.responseWait()
+        logger.info(msg)
+
+    def encodeCmd(self, cmd):
         return cmd.encode(self._file_encode, 'ignore')
 
-    def decodeCmd(self,cmd):
-        return cmd.decode(self._file_encode,'ignore')
+    def decodeCmd(self, cmd):
+        return cmd.decode(self._file_encode, 'ignore')
 
     def sendStartWriteSd(self):
         cmd = self.CMD_STARTWRITE_SD + self.fileName
         logger.info("Start write to SD: " + cmd)
-        self.sock.sendto(self.encodeCmd(cmd), (self.ipaddr, self.PORT))
-        message, address = self.sock.recvfrom(self.RECVBUF)
-        message = message.decode('utf-8','replace')
-        logger.info("Start write to SD result: " +  message)
+        self.sock.send(self.encodeCmd(cmd))
+        msg = self.responseWait()
+        logger.info("Start write to SD result: " + msg)
         return
 
+    def status(self):
+        self.sock.send(bytes(self.CMD_STATUS, self._file_encode))
+        msg = self.responseWait()
+        logger.debug(msg)
+        return msg
+
     def sendEndWriteSd(self):
-        cmd = self.CMD_ENDWRITE_SD + self.fileName
+        cmd = self.CMD_ENDWRITE_SD + self.CMD_TERM
         logger.info("End write to SD: " + cmd)
-        self.sock.sendto(self.encodeCmd(cmd), (self.ipaddr, self.PORT))
-        message, address = self.sock.recvfrom(self.RECVBUF)
-        message = message.decode('utf-8','replace')
-        logger.info("End write to SD result: " +  message)
+        self.sock.send(self.encodeCmd(cmd))
+        msg = self.responseWait()
+        # TODO: Add code to confirm that the file was received and closed properly
+        logger.info("End write to SD result: " + msg)
         return
 
     def sendGetFileList(self):
-        cmd = self.CMD_GETFILELIST
+        # TODO: Figure out how file listing works on FF Printers.
+        cmd = self.CMD_GETFILELIST + self.CMD_TERM
         logger.info("Get List of Files: " + cmd)
-        self.sock.sendto(self.encodeCmd(cmd), (self.ipaddr, self.PORT))
-        message, address = self.sock.recvfrom(self.RECVBUF)
-        message = message.decode('utf-8','replace')
-        logger.info("Get list of files result: " +  message)
-        return
+        self.sock.send(self.encodeCmd(cmd))
+        msg = self.responseWait()
+        logger.info("Get list of files result: " + msg)
+        return msg
 
-    def addCheckSum(self, data, seekPos):
+    def sendFileChunk(self, buff, seekPos, chknum):
 
-        seekArray = struct.pack('>I', seekPos)
+        # logger.debug("File Position: " + str(seekPos))
 
-        check_sum = 0
-        data += b"000000"
-        dataArray = bytearray(data)
+        chunkCount = chknum.to_bytes(4,'big')
+        myCRC = binascii.crc32(buff).to_bytes(4, 'big')
+        dataArray = self.CHUNK_HEADER1 + chunkCount + self.CHUNK_HEADER2 + myCRC + buff
+        tmpSize = len(dataArray)
 
-        datSize = len(dataArray) - 6
-
-        if datSize <= 0:
-            return
-
-        dataArray[datSize] = seekArray[3]
-        dataArray[datSize + 1] = seekArray[2]
-        dataArray[datSize + 2] = seekArray[1]
-        dataArray[datSize + 3] = seekArray[0]
-
-        for i in range(0, datSize+4, 1):
-            check_sum ^= dataArray[i]
-
-        dataArray[datSize + 4] = check_sum
-        dataArray[datSize + 5] = 0x83
-
-        return dataArray
-
-    def sendFileChunk(self, buff, seekPos):
-
-        logger.info("File Position: " + str(seekPos))
-        tmpArray = bytearray(buff)
-        tmpSize = len(tmpArray)
         if tmpSize <= 0:
             return
 
-        dataArray = self.addCheckSum(buff, seekPos)
-
-        datSize = len(dataArray) - 6
-
-        if datSize <= 0:
-            logger.warning('Error computing checksum: Data size is 0')
-            return
-
-        self.sock.sendto(dataArray, (self.ipaddr, self.PORT))
-
-        message, address = self.sock.recvfrom(self.RECVBUF)
-        message = message.decode('utf-8','replace')
-        logger.info("Sending File Chunk result: " +  message)
+        self.sock.send(dataArray)
+        msg = self.responseWait()
+        if not self.responseOK(msg):
+            logger.error("Failed to get OK from printer to chunck")
+            return -1
 
         return
-
 
     def sendFile(self):
 
         with open(self.fileName, 'rb', buffering=1) as fp:
+            # Get File Length
+            fp.seek(0, 2)
+            filesize = fp.tell()
+            fp.seek(0, 0)
+            logger.info("Filesize is: " + str(filesize))
+
+            # Announce to the printer that we are sending a file.
+            cmd = '~M28 ' + str(filesize) + ' 0:/user/' + self.baseFilename + self.CMD_TERM
+            self.sock.send(self.encodeCmd(cmd))
+            msg = self.responseWait()
+            if not self.responseOK(msg):
+                logger.error("Printer failed to accept file")
+                exit(-1)
+
+            logger.info("Printer ready to recieve file: " + cmd)
+            # Chunk File up into lots of 4092 packets for sending
+            # Stick header on each chunk.
+            chunkNum = 0
+            chunkHeaderLength = len(self.CHUNK_HEADER1) + len(self.CHUNK_HEADER2) + 4
             while True:
                 seekPos = fp.tell()
-                chunk = fp.read(self.BUFSIZE)
+                chunk = fp.read(self.BUFSIZE - chunkHeaderLength - 5)
+                # logger.debug("Bytes Read from file " + str(len(chunk)))
                 if not chunk:
                     break
 
-                self.sendFileChunk(chunk, seekPos)
+                if len(chunk) < self.BUFSIZE-chunkHeaderLength-5:
+                    logger.debug("Appended \\x00's to make up buffer")
+                    chunk += bytearray(self.BUFSIZE-chunkHeaderLength-5-len(chunk))
 
-        logger.info("End write SendFile ")
+                self.sendFileChunk(chunk, seekPos, chunkNum)
+                chunkNum += 1
+
+        logger.debug("End write SendFile ")
         fp.close()
+
+        self.sendEndWriteSd()
 
         return
 
+    def sendStartPrint(self):
+        sdFileName = self.sdPath + self.baseFilename
+        cmd = self.CMD_SETSDFILE + sdFileName + self.CMD_TERM
+        self.sock.send(self.encodeCmd(cmd))
+        msg = self.responseWait()
+        if self.responseOK(msg):
+            logger.debug("startPrint: set sd card filename: " + sdFileName)
+        else:
+            logger.error("startPrint: failed to set SD card filename: " + sdFileName)
+            return False
 
-    def dataCompressThread(self):
-        logger.info("Compressing Gcode File")
-        self.datamask = '[0-9]{1,12}\.[0-9]{1,12}'
-        self.maxmask = '[0-9]'
-        tryCnt = 0
-        while True:#(send_ip):
-            try:
-#                self.sock.settimeout(2)
-#                Logger.log("i", self._targetIP)
-                self.sock.sendto(b"M4001", (self.ipaddr, self.PORT))
-                message, address = self.sock.recvfrom(self.BUFSIZE)
-                pattern = re.compile(self.datamask)
-                msg = message.decode('utf-8','ignore')
-                if('X' not in msg or 'Y' not in msg or 'Z' not in msg ):
-                    continue
-                msg = msg.replace('\r','')
-                msg = msg.replace('\n', '')
-                msgs = msg.split(' ')
-                logger.info(msg)
-                e_mm_per_step = z_mm_per_step = y_mm_per_step = x_mm_per_step = '0.0'
-                s_machine_type = s_x_max = s_y_max = s_z_max = '0.0'
-                for item in msgs:
-                    _ = item.split(':')
-                    if(len(_) == 2):
-                        id = _[0]
-                        value = _[1]
-                        logger.info(_)
-                        if id == 'X':
-                            x_mm_per_step = value
-                        elif id == 'Y':
-                            y_mm_per_step = value
-                        elif id == 'Z':
-                           z_mm_per_step = value
-                        elif id == 'E':
-                            e_mm_per_step = value
-                        elif id == 'T':
-                            _ = value.split('/')
-                            if len(_) == 5:
-                                s_machine_type = _[0]
-                                s_x_max = _[1]
-                                s_y_max = _[2]
-                                s_z_max = _[3]
-                        elif id == 'U':
-                            self._file_encode = value.replace("'","")
-                cmd = path.normpath(".\VC_compress_gcode.exe") + " \"" + self.gcodeFile + "\" " + x_mm_per_step + " " + y_mm_per_step + " " + z_mm_per_step + " " + e_mm_per_step\
-                         + ' \"' + path.normpath(".") + '\" ' + s_x_max + " " + s_y_max + " " + s_z_max + " " + s_machine_type
-                logger.info(cmd)
-                ret = subprocess.Popen(cmd,stdout=subprocess.PIPE,shell=True)
-                logger.info(ret.stdout.read().decode("utf-8", 'ignore'))
-                break
-            except timeout:
-                tryCnt += 1
-                if(tryCnt > 5):
-                    self._result = MyWifiSend.CONNECT_TIMEOUT
-                    break
-            except:
-                traceback.print_exc()
-                break
+        cmd = self.CMD_STARTPRINT + self.CMD_TERM
+        self.sock.send(self.encodeCmd(cmd))
+        msg = self.responseWait()
+        if self.responseOK(msg):
+            logger.debug("startPrint: successfully started print job")
+        else:
+            logger.error("startPrint: failed to start print job")
+            return False
 
-    def startPrint(self):
-        self.sock.settimeout(2)
-        try:
-            cmd = 'M6030 ":' + self.fileName + '" I1'
-            self.sock.sendto(self.encodeCmd(cmd), (self.ipaddr, self.PORT))
-            message, address = self.sock.recvfrom(self.RECVBUF)
-        except:
-            traceback.print_exc()
+        return True
 
-def ReadFileChunk(filename, startPos, endPos):
+    def responseWait(self):
+        msg = self.sock.recv(self.RECVBUF)
+        return msg.decode(self._file_encode, 'replace')
+
+    def responseOK(self, response):
+        # TODO: Make this do a more robust response check.
+
+        logger.debug(response)
+        logger.debug(response.endswith((str(self.RESPONSE_OK))))
+        if response.endswith(str(self.RESPONSE_OK)):
+            logger.debug("Server Response: OK")
+            return True
+        else:
+            logger.debug( "Fuck It. \n'" + response + "'")
+            logger.error("Server Response: NOT OK")
+            return False
 
 
-    return
+def __Main__():
+
+    # Commands line arguments
+    argpar = argparse.ArgumentParser()
+    argpar.add_argument('filename', nargs='?', action='store', help='Filename of file to send to printer.')
+    argpar.add_argument('-s', action='store_true', help='Print status of the 3D Printer.')
+    argpar.add_argument('-l', action='store_true', help='List the files on the printer SD Card.')
+    argpar.add_argument('-p', action='store_true', help='Print on completion of file download.')
+    argpar.add_argument('-a', dest='hostname', default='10.1.1.129', help='Hostname or IP address of printer.')
+
+    # Create the printer object ready to start recieving variables.
+    myobj = Print3D()
+
+    args = argpar.parse_args()
+
+    # Connect to the host
+    myobj.connect(args.hostname)
+
+    if args.s:
+        status = myobj.status()
+        print(status)
+
+    if args.l:
+        print(myobj.sendGetFileList())
+
+    if args.filename:
+        myFile = Path(args.filename)
+        myobj.baseFilename = myFile.name
+
+        if myFile.is_file:
+            myFile = Path(args.filename).resolve()
+            myobj.fileName = str(myFile)
+            print("Sending to printer: " + myobj.filename)
+            myobj.sendFile()
+
+        if args.p:
+            myobj.sendStartPrint()
+            print("Set to print: " + myobj.baseFilename)
+
+
+    myobj.release()
 
 
 if __name__ == '__main__':
-    printDev = WiFiDevice()
-    printDev.dirPath = os.path.dirname(os.path.realpath(__file__))
-    os.chdir(printDev.dirPath)
-    printDev.ipaddr = sys.argv[1]
-    printDev.name = 'Xpro'
-    printDev.gcodeFile = sys.argv[2]
-    printDev.fileName = printDev.gcodeFile + '.tz'   
-    logger.info('File 1: ' +  printDev.gcodeFile + 'File 2: ' + printDev.fileName)
-    printDev.dataCompressThread()
-    time.sleep(2)
-    logger.info('3D Printer ' + printDev.name + ' IP address: ' + printDev.ipaddr)
-    printDev.sendStartWriteSd()
-    time.sleep(2)
-    printDev.sendFile()
-    printDev.sendEndWriteSd()
-    if len(sys.argv) >= 4 and sys.argv[3] == 'yes':
-        printDev.startPrint()
+    __Main__()
